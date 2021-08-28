3ee379bd1e8c6131e302e6d1862a28c2b31bee8d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-08-28 12:12:33 +0300

    Fetching an image from todo-backend works in todo-frontend... Although not sure it works as it should.

--- todo-app/frontend/src/components/Image.vue
+++ todo-app/frontend/src/components/Image.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>A random image</h1>
-  <!-- <img src="http://localhost:8081/api/image" /> -->
-  <img src="http://todo-backend-svc/api/image" />
+  <img src="http://localhost:8081/api/image" />
+  <!-- <img src="http://todo-backend-svc/api/image" /> -->
 </template>
 
 <script lang="ts">
