36bee5e27b5bc97a7085a33d29f0644d7ba96d4f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-09-02 14:28:13 +0300

    ex 2 done

--- todo-app/frontend/src/environment.ts
+++ todo-app/frontend/src/environment.ts
@@ -1,6 +1,6 @@
 // Can't be bothered to figure out how environment variables work in Vue
 
-const port = 8080;
+const port = 8081;
 
 // eslint-disable-next-line import/prefer-default-export
 export const backendUrl = `http://localhost:${port}/api/`;
