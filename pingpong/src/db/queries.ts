5e7eaed2fa062a2c154f3407d4ae65d89812c04e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-09-04 18:53:05 +0300

    Move db logic to queries.ts

---
+++ pingpong/src/db/queries.ts
@@ -0,0 +1,24 @@
+import pool from './pool';
+
+const insertPing = async (timestamp: string): Promise<void> => {
+  pool.query('insert into pings (timestamp) values ($1)', [timestamp], (error, results) => {
+    if (error) {
+      console.log('error querying pings', error);
+      throw error;
+    }
+    console.log('results', results);
+  });
+};
+
+const pingCount = async (): Promise<void> => {
+  pool.query('SELECT COUNT(*) FROM pings', (error, results) => {
+    if (error) {
+      console.log('error getting ping count', error);
+      throw error;
+    }
+
+    console.log('ping count', results);
+  });
+};
+
+export default { insertPing, pingCount };
--- pingpong/src/services/pingService.ts
+++ pingpong/src/services/pingService.ts
@@ -1,29 +1,8 @@
 import { Response } from 'express';
 import pool from '../db/pool';
+import queries from '../db/queries';
 import timestampService from './timestampService';
 
-const insertPing = async (timestamp: string): Promise<void> => {
-  pool.query('insert into pings (timestamp) values ($1)', [timestamp], (error, results) => {
-    if (error) {
-      console.log('error querying pings', error);
-      throw error;
-    }
-    console.log('results', results);
-  });
-};
-
-const pingCount = async (): Promise<void> => {
-  pool.query('SELECT COUNT(*) FROM pings', (error, results) => {
-    if (error) {
-      console.log('error getting ping count', error);
-      throw error;
-    }
-
-    console.log('ping count', results);
-  });
-};
-
-// Exported functions
 const getPings = async (res: Response): Promise<void> => {
   pool.query('SELECT * FROM pings', (error, results) => {
     if (error) {
@@ -36,8 +15,8 @@ const getPings = async (res: Response): Promise<void> => {
 
 const savePing = async (): Promise<void> => {
   const timestamp = timestampService.getCurrentTimeStamp();
-  await insertPing(timestamp);
-  await pingCount();
+  await queries.insertPing(timestamp);
+  await queries.pingCount();
 };
 
 export default { getPings, savePing };
