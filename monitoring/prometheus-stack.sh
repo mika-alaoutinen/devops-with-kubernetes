f9f21868496a80c27d450b85dce7ce07f0b30f7d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-09-11 10:35:00 +0300

    Add Prometheus stack to ex 2.10

---
+++ monitoring/prometheus-stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Installs Prometheus stack on namespace "prometheus"
+
+# Add Prometheus repository
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+# Install Prometheus stack as "prometheus"
+helm install prometheus prometheus-community/kube-prometheus-stack -n prometheus
+
+# Port forward Grafana to 3000 on localhost. The service pod has to be ready for this to work.
+kubectl -n prometheus port-forward svc/prometheus-grafana 3000:80
