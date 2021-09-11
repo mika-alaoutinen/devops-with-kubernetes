df3cbe3d81cffc0f53bbad64aea0659306ff9d15
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-09-11 09:27:15 +0300

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Sets up a k3d cluster
+
+k3d cluster create --port '8082:30080@agent[0]' -p 8081:80@loadbalancer --agents 2
+
+kubectl create ns main
+kubectl create ns todo
+kubectl create ns monitoring
+
+kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.12.1/controller.yaml
