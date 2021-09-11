0f8152ba9e06c92e1ef1bd45deba4c1781321e2e
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-09-11 11:07:38 +0300

    Ex 3

--- monitoring/prometheus-stack.sh
+++ monitoring/prometheus-stack.sh
@@ -5,8 +5,8 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-# Install Prometheus stack as "prometheus"
-helm install prometheus prometheus-community/kube-prometheus-stack -n prometheus
+# Install Prometheus stack as "prometheus-stack"
+helm install prometheus-stack prometheus-community/kube-prometheus-stack -n prometheus
 
 # Port forward Grafana to 3000 on localhost. The service pod has to be ready for this to work.
-kubectl -n prometheus port-forward svc/prometheus-grafana 3000:80
+kubectl -n prometheus port-forward svc/prometheus-stack-grafana 3000:80
