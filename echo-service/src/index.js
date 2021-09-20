a22fa975cb1cb9be51f345305c9a104a4a662cc7
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2021-09-20 18:09:05 +0300

    Ex 6 done

--- echo-service/src/index.js
+++ echo-service/src/index.js
@@ -3,10 +3,21 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const messages = [];
+
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log('this is echo service');
+});
+
+app.get('/echo', (req, res) => {
+  res.send(messages);
+})
 
 app.post('/echo', (req, res) => {
   console.log(req.body);
+  messages.push(req.body);
   res.send(req.body);
 });
 
--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# Sets up a k3d cluster
 
+# Set up k3d cluster
 k3d cluster create --port '8082:30080@agent[0]' -p 8081:80@loadbalancer --agents 2
 
+# Create namespaces
 kubectl create ns main
 kubectl create ns todo
+kubectl create ns argo-rollouts
 
+# Add sealed secrets
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.12.1/controller.yaml
+
+# Add Argo Rollouts
+kubectl apply -n argo-rollouts -f https://raw.githubusercontent.com/argoproj/argo-rollouts/stable/manifests/install.yaml
